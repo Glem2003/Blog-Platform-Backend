af39cbc3a8f5ffb717d8fc752f217d5c2246e582
Glem2003 <glem920813@gmail.com>
2024-12-27 23:44:11 +0800

    update

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -4,3 +4,8 @@ import json
 def read_json(DATA_FILE):
     with open(DATA_FILE, 'r') as file:
         return json.load(file)
+
+
+def write_json(DATE_FILE, data):
+    with open(DATE_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
