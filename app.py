0497b34e112f380a71cb94ed61ca3b68a410dc09
Glem2003 <glem920813@gmail.com>
2024-12-28 11:13:24 +0800

    update add CORS

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 from router.api_routes import api_routes
 
 
 def blog_platform():
     app = Flask(__name__)
+    CORS(app)
 
     # RESTful API
     api_routes(app)
 
     return app
+
 
 app = blog_platform()
